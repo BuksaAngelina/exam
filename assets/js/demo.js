ba5a635dd64be365a138bfad00a1474c69093fdf
angelina <angelinabuksa387@gmail.com>
2025-06-20 04:53:31 +0300

    feat: impl squer and coal show

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -6,12 +6,14 @@ const state = {
 
 function onChangeShapeSides(value) {
     document.getElementById('sides-label').textContent = value;
+    document.getElementById('coal-value').textContent = Math.round((value-2) * 180 / value*100)/100;
     state.sides = value;
     drawShapeFromState();
 }
 
 function onChangeShapeRadius(value) {
     state.radius = value;
+    document.getElementById('edge-value').textContent = value;
     drawShapeFromState();
 }
 
@@ -26,6 +28,7 @@ function onChangeShapeShowCoalsRevers() {
 }
 
 function drawShapeFromState() {
+    document.getElementById('squre-value').textContent = Math.round(state.sides * state.radius*state.radius / 4 * (1 / Math.tan((180 * Math.PI) / 180/state.sides))*100)/100;
     document.querySelector('#shape-presentation')
         .setAttribute('regular-ngon', `sides: ${state.sides}; radius: ${state.radius}; highlightCoals: ${state.showCoals}`);
 }
